88c4b28cbfe1b1adffc5cf51726f3a2715e1a777
mpetavy <mpetavy>
2025-05-28 09:32:50 +0200

    Support CSV input

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"bytes"
 	"embed"
+	"encoding/csv"
 	"flag"
 	"github.com/atotto/clipboard"
 	"github.com/mpetavy/common"
@@ -13,6 +14,7 @@ import (
 
 var (
 	filename = flag.String("f", "", "filename")
+	readCsv  = flag.Bool("csv", false, "read as csv")
 	format   = flag.String("format", "markdown", "format of output 'markdown', 'table', 'html'")
 )
 
@@ -51,109 +53,131 @@ func run() error {
 
 	st := common.NewStringTable()
 
-	crlf, err := common.NewSeparatorSplitFunc(nil, []byte("\n"), false)
-	if common.Error(err) {
-		return err
-	}
-
 	output := strings.Builder{}
-	cell := strings.Builder{}
 
-	inMyTable := false
+	if *readCsv || strings.Contains(string(ba), ",") {
+		common.Info("Reading as CSV")
+		common.Info("")
 
-	scanner := bufio.NewScanner(bytes.NewReader(ba))
-	scanner.Split(crlf)
-	for scanner.Scan() {
-		line := scanner.Text()
+		c := csv.NewReader(bytes.NewReader(ba))
+		recs, err := c.ReadAll()
+		if common.Error(err) {
+			return err
+		}
 
-		switch {
-		case strings.HasPrefix(line, "++TABLE"):
-			if inMyTable && cell.Len() > 0 {
+		for row := range len(recs) {
+			st.AddRow()
+			for col := range len(recs[row]) {
+				st.AddCol(recs[row][col])
+			}
+		}
+	} else {
+		common.Info("Reading as strings")
+		common.Info("")
+
+		crlf, err := common.NewSeparatorSplitFunc(nil, []byte("\n"), false)
+		if common.Error(err) {
+			return err
+		}
+
+		cell := strings.Builder{}
+
+		inMyTable := false
+
+		scanner := bufio.NewScanner(bytes.NewReader(ba))
+		scanner.Split(crlf)
+		for scanner.Scan() {
+			line := scanner.Text()
+
+			switch {
+			case strings.HasPrefix(line, "++TABLE"):
+				if inMyTable && cell.Len() > 0 {
+					st.AddCol(cell.String())
+				}
+
+				cell.Reset()
+
+				inMyTable = !inMyTable
+
+				if inMyTable {
+					st.NoHeader = true
+				} else {
+					if st.Rows() > 0 {
+						output.WriteString(st.Markdown())
+
+						st.Clear()
+						st.NoHeader = false
+					}
+				}
+
+				continue
+			case strings.HasPrefix(line, "++ROW"):
+				if cell.Len() > 0 {
+					st.AddCol(cell.String())
+				}
+
+				cell.Reset()
+
+				st.AddRow()
+
+				continue
+			case strings.HasPrefix(line, "++COL"):
+				if cell.Len() == 0 {
+					cell.WriteString("<na>")
+				}
+
 				st.AddCol(cell.String())
+
+				cell.Reset()
+
+				continue
 			}
 
-			cell.Reset()
-
-			inMyTable = !inMyTable
-
 			if inMyTable {
-				st.NoHeader = true
-			} else {
+				if cell.Len() > 0 {
+					cell.WriteString("<br>")
+				}
+
+				cell.WriteString(strings.TrimSpace(line))
+
+				continue
+			}
+
+			if !strings.Contains(line, "|") {
 				if st.Rows() > 0 {
 					output.WriteString(st.Markdown())
 
 					st.Clear()
 					st.NoHeader = false
 				}
-			}
 
-			continue
-		case strings.HasPrefix(line, "++ROW"):
-			if cell.Len() > 0 {
-				st.AddCol(cell.String())
-			}
+				output.WriteString(line)
 
-			cell.Reset()
-
-			st.AddRow()
-
-			continue
-		case strings.HasPrefix(line, "++COL"):
-			if cell.Len() == 0 {
-				cell.WriteString("<na>")
-			}
-
-			st.AddCol(cell.String())
-
-			cell.Reset()
-
-			continue
-		}
-
-		if inMyTable {
-			if cell.Len() > 0 {
-				cell.WriteString("<br>")
-			}
-
-			cell.WriteString(strings.TrimSpace(line))
-
-			continue
-		}
-
-		if !strings.Contains(line, "|") {
-			if st.Rows() > 0 {
-				output.WriteString(st.Markdown())
-
-				st.Clear()
-				st.NoHeader = false
-			}
-
-			output.WriteString(line)
-
-			continue
-		}
-
-		line = strings.TrimSpace(line)
-
-		if strings.HasPrefix(line, "|") {
-			line = line[1:]
-		}
-
-		if strings.HasSuffix(line, "|") {
-			line = line[:len(line)-1]
-		}
-
-		if len(line) > 0 && !strings.Contains(line, "---") {
-			splits := common.Split(line, "|")
-
-			if len(splits) == 0 {
 				continue
 			}
 
-			st.AddRow()
-			for _, split := range splits {
-				split = strings.TrimSpace(strings.ReplaceAll(split, "|", ""))
-				st.AddCol(strings.TrimSpace(split))
+			line = strings.TrimSpace(line)
+
+			if strings.HasPrefix(line, "|") {
+				line = line[1:]
+			}
+
+			if strings.HasSuffix(line, "|") {
+				line = line[:len(line)-1]
+			}
+
+			if len(line) > 0 && !strings.Contains(line, "---") {
+				splits := common.Split(line, "|")
+
+				if len(splits) == 0 {
+					continue
+				}
+
+				st.AddRow()
+				for _, split := range splits {
+					split = strings.TrimSpace(strings.ReplaceAll(split, "|", ""))
+					st.AddCol(strings.TrimSpace(split))
+				}
 			}
 		}
 	}
